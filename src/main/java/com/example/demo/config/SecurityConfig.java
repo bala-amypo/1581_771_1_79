94f398b3064b154bf2bf732670ad6086197aa35f
bala-amypo <bala@amypo.in>
2025-12-18 08:57:25 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,13 +1,7 @@
-package com.example.demo;
+package com.example.demo.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+@Configuration
+public class SecurityConfig {
 }
