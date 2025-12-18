94bf489ebfa58688c3dd20d899d34b5307298109
bala-amypo <bala@amypo.in>
2025-12-18 06:08:52 +0000

    User commit

--- src/main/java/com/example/demo/model/Shipment.java
+++ src/main/java/com/example/demo/model/Shipment.java
@@ -1,10 +1,8 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import java.time.LocalDate;
-
 @Entity
 @Table(name = "shipments")
 @Data
@@ -12,30 +10,24 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class Shipment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "vehicle_id", nullable = false)
     @NotNull
     private Vehicle vehicle;
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "pickup_location_id", nullable = false)
     @NotNull
     private Location pickupLocation;
-
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "drop_location_id", nullable = false)
     @NotNull
     private Location dropLocation;
-
     @NotNull
     @Positive
     private Double weightKg;
-
     @NotNull
     private LocalDate scheduledDate;
 }
