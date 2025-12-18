5bc79d261c6b47d887da7faa4cd2b37c97a9445c
bala-amypo <bala@amypo.in>
2025-12-18 09:15:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Location;
+import com.example.demo.repository.LocationRepository;
+import com.example.demo.service.LocationService;
+
+@Service
+public class LocationServiceImpl implements LocationService {
+
+    private final LocationRepository repo;
+
+    public LocationServiceImpl(LocationRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Location createLocation(Location location) {
+        if (location.getLatitude() > 90 || location.getLatitude() < -90) {
+            throw new IllegalArgumentException("Invalid latitude value");
+        }
+        return repo.save(location);
+    }
+
+    @Override
+    public List<Location> getAllLocations() {
+        return repo.findAll();
+    }
+}
