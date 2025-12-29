96cd9c2c65ce0fa80ecea9cfb564b572ed59f539
bala-amypo <bala@amypo.in>
2025-12-29 09:51:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,37 +1,3 @@
-// package com.example.demo.entity;
-
-// import jakarta.persistence.*;
-// import lombok.*;
-
-// import java.time.LocalDateTime;
-
-// @Entity
-// @Table(name = "route_optimization_results")
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
-// @Builder
-// public class RouteOptimizationResult {
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     @ManyToOne
-//     @JoinColumn(name = "shipment_id", nullable = false)
-//     private Shipment shipment;
-
-//     private Double optimizedDistanceKm;
-
-//     private Double estimatedFuelUsageL;
-
-//     private LocalDateTime generatedAt;
-
-//     @PrePersist
-//     protected void onCreate() {
-//         this.generatedAt = LocalDateTime.now();
-//     }
-// }
 package com.example.demo.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
